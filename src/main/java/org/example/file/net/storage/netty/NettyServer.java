c90452aee0ed3433447594e89331fa565569c97d
Andrew <sherlockh989@gmail.com>
2022-05-15 15:04:10 +0300

    Base

--- src/main/java/org/example/file/net/storage/netty/NettyServer.java
+++ src/main/java/org/example/file/net/storage/netty/NettyServer.java
@@ -1,4 +1,34 @@
 package org.example.file.net.storage.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class NettyServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap(); // (2)
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast();
+                        }
+                    }); // (5) (6)
+            ChannelFuture f = serverBootstrap.bind(45001).sync(); //(7)
+            f.channel().closeFuture().sync();
+        }finally {
+            workerGroup.shutdownGracefully();
+           bossGroup.shutdownGracefully();
+        }
+    }
 }
